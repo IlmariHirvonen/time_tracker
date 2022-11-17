e4f0aac499c1e90af167f6ffcb0bf44c78e2a5de
IlmariHirvonen <ilmarinukkeli@gmail.com>
2022-11-17 19:05:36 +0200

    removed test.csv and added a cool spinner

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from win32gui import GetWindowText, GetForegroundWindow
 import time
 import csv
 from ctypes import Structure, windll, c_uint, sizeof, byref
+import sys
+from datetime import datetime
+import os
+from spinner import Spinner
+import keyboard
 
 
 class LASTINPUTINFO(Structure):
@@ -9,7 +14,6 @@ class LASTINPUTINFO(Structure):
         ('cbSize', c_uint),
         ('dwTime', c_uint),
     ]
-
 def get_idle_duration():
     lastInputInfo = LASTINPUTINFO()
     lastInputInfo.cbSize = sizeof(lastInputInfo)
@@ -17,15 +21,21 @@ def get_idle_duration():
     millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
     return millis / 1000.0
 
-from datetime import datetime
-datetime.fromisoformat('2011-11-04T00:05:23')
-import os
 cwd = os.getcwd() + "/test.csv"
-print(cwd)
-with open(cwd, "a") as f:
-    writer = csv.writer(f)
-    while(True):
-        writer.writerow([datetime.now(),GetWindowText(GetForegroundWindow()),get_idle_duration()])
-        time.sleep(15)
+datetime.fromisoformat('2011-11-04T00:05:23')
 
+def main():
+    try:
+        with open(cwd, "a") as f:
+            print("Press ctrl+c to quit.\nCollecting data...")
+            with Spinner():
+                writer = csv.writer(f)
+                while True:
+                    writer.writerow([datetime.now(),GetWindowText(GetForegroundWindow()),get_idle_duration()])
+                    time.sleep(15)
+    except KeyboardInterrupt:
+        print("Exiting...")
+    sys.exit(0)
 
+if __name__ == "__main__":
+    main()
